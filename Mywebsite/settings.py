17dfb4f72b3545eafc92a9ebbab7bbf731c65cce
daniel <danielepplin001@gmail.com>
2020-10-14 11:33:20 -0700

    intial commit

--- Mywebsite/settings.py
+++ Mywebsite/settings.py
@@ -137,10 +137,7 @@ STATICFILES_DIRS = [BASE_DIR, 'Static'
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
- 
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
  
